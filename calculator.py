e2314b68c2b8fb68466c8f62089a3dd556c80bc0
xXx_NOT_FOUND_xXx <63779958+hamalord4444@users.noreply.github.com>
2021-09-08 22:34:12 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,38 @@
+# Created by xXx_not_found_xXx
+
+print('''
+[1] korkrdnawa , 
+[2] ledarkrdn , 
+[3] karat , 
+[4] dabash''')
+print('')
+option = input('hallbzhera > ')
+print('')
+
+# kokrdnawa
+if option == '1':
+   i1 = input('zhmaray yakam > ')
+   i2 = input('zhmaray dwam > ')
+   sum = int(i1) + int(i2)
+   print(sum)
+   
+# lekdan
+if option == '2':
+   i1 = input('zhmaray yakam > ')
+   i2 = input('zhmaray dwam > ')
+   sum = int(i1) - int(i2)
+   print(sum)
+ 
+ # karat
+if option == '3':
+   i1 = input('zhmaray yakam > ')
+   i2 = input('zhmaray dwam > ')
+   sum = int(i1) * int(i2)
+   print(sum)
+ 
+ # dabash
+if option == '4':
+   i1 = input('zhmaray yakam > ')
+   i2 = input('zhmaray dwam > ')
+   sum = int(i1) / int(i2)
+   print(sum)
